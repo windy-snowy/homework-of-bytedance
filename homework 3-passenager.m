a909b02ed7155621fdd0a371e417c7b0e08db5bd
windy-snowy <109153341+windy-snowy@users.noreply.github.com>
2022-08-01 00:00:55 +0800

    Update homework 3-passenager.m

--- homework 3-passenager.m
+++ homework 3-passenager.m
@@ -30,31 +30,30 @@
 }
 
 -void InOrderswith   date: (NSdate*)date  name :NSString* name{
-orders * neworderpointer = order_th + self.neworder;
-self.neworderpointer->date = date;
-self.neworderpointer->name = name;
-self.neworderspointer->have = 1;
-irder_th++;
+    orders * neworderpointer = order_th + self.neworder;
+    self.neworderpointer->date = date;
+    self.neworderpointer->name = name;
+    self.neworderspointer->have = 1;
+    irder_th++;
 }
+
 -void OutOrderswith date:(NSdate*)date  name :NSString* name{
+    self.neworderpointer->date = 0;
+    self.neworderpointer->name = 0;
+    self.neworderspointer->have =  0;
+    for(NSnumber ;;)
+    {
+        self.neworderpointer->date = date;
+        self.neworderpointer->name = name;
+        self.neworderspointer->have = 1;
+    }
+    inorder_th--;
 
-self.neworderpointer->date = 0;
-self.neworderpointer->name = 0;
-self.neworderspointer->have =  0;
-for(NSnumber ;;)
-{
-self.neworderpointer->date = date;
-self.neworderpointer->name = name;
-self.neworderspointer->have = 1;
-}
-inorder_th--;
+    self.oldorder->date = date;
+    self.oldorder->name = name;
 
-
-self.oldorder->date = date;
-self.oldorder->name = name;
-
-self.oldorders->have = 0;
-outorder_th++;
+    self.oldorders->have = 0;
+    outorder_th++;
 }
 
 @end
